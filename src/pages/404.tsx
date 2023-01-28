31b905797adc2ed6126bb58e9ca345082d668643
daniel-dunsin <adejaredaniel12@gmail.com>
2023-01-28 19:06:14 +0000

    added 404 page and fixed filter box state

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FaExclamationTriangle } from "react-icons/fa";
 
 const Error404 = () => {
-  return <div>Error404</div>;
+  return (
+    <div className="w-full bg-lightBg text-lightText dark:bg-darkBg dark:text-darkText min-h-screen px-8 flex justify-center items-center flex-col gap-y-6">
+      <span className="text-[30px] text-red-600">
+        <FaExclamationTriangle />
+      </span>
+      <h1>The requested page doesn't exist</h1>
+      <Link
+        to={"/"}
+        className="p-3 bg-red-600 text-white rounded-md shadow-md hover:shadow-lg"
+      >
+        Return to homepage
+      </Link>
+    </div>
+  );
 };
 
 export default Error404;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -19,15 +19,13 @@ const Home = () => {
   useEffect(() => {
     getCountries();
   }, []);
-  if (loading) {
-    return (
-      <div className="w-full bg-lightBg text-lightText dark:bg-darkBg dark:text-darkText min-h-screen px-8 flex justify-center items-center">
-        <span className="text-[35px] text-[royalBlue] animate-spin">
-          <FaSpinner />
-        </span>
-      </div>
-    );
-  }
+  // if () {
+  //   return (
+  //     <div className="w-full bg-lightBg text-lightText dark:bg-darkBg dark:text-darkText min-h-screen px-8 flex justify-center items-center">
+
+  //     </div>
+  //   );
+  // }
   if (error && errorText.toString() === "404") {
     return (
       <>
@@ -49,65 +47,72 @@ const Home = () => {
         {/* search box */}
         <Header />
       </div>
-      <div className="mt-[14px] flex flex-wrap gap-[2rem] justify-center items-center max-w-[1200px] mx-auto">
-        {countries?.slice(0, 28).map((country, index: number) => {
-          return (
-            <Link
-              key={index}
-              to={`/country/${country.name.common}`}
-              className="w-full md:flex-[32%] lg:flex-[23%] max-w-[300px] shadow-md rounded-md overflow-hidden"
-            >
-              <article>
-                {/* image container */}
-                <header className="w-full h-[180px]">
-                  <img
-                    src={country.flags.png}
-                    alt={country.name.common}
-                    loading="lazy"
-                    className="w-full h-full object-cover object-center"
-                  />
-                </header>
-                <main className="bg-white text-lightText dark:bg-darkElement dark:text-darkText p-6">
-                  <h2 className="text-center font-bold text-[17px] mb-2">
-                    {country.name.common}
-                  </h2>
-                  {country?.population! && (
-                    <div className={styles.flexItems}>
-                      <p className="font-bold">Population: </p>
-                      <p>{country.population.toLocaleString() as string}</p>
-                    </div>
-                  )}
-                  {country?.region && (
-                    <div className={styles.flexItems}>
-                      <p className="font-bold">Region: </p>
-                      <p>{country.region}</p>
-                    </div>
-                  )}
-                  {country?.capital?.length > 0 && (
-                    <div className={styles.flexItems}>
-                      <p className="font-bold">Capital: </p>
-                      {country?.capital?.map(
-                        (capital: string, index: number) => {
-                          return (
-                            <p key={index}>
-                              {capital}
-                              {index === country.capital.length - 1 ? (
-                                ""
-                              ) : (
-                                <span className="ml-2">,</span>
-                              )}
-                            </p>
-                          );
-                        }
-                      )}
-                    </div>
-                  )}
-                </main>
-              </article>
-            </Link>
-          );
-        })}
-      </div>
+      {loading && (
+        <span className="text-[35px] text-[royalBlue] animate-spin w-full flex justify-center items-center">
+          <FaSpinner />
+        </span>
+      )}
+      {!loading && (
+        <div className="mt-[14px] flex flex-wrap gap-[2rem] justify-center items-center max-w-[1200px] mx-auto">
+          {countries?.slice(0, 28).map((country, index: number) => {
+            return (
+              <Link
+                key={index}
+                to={`/country/${country.name.common}`}
+                className="w-full md:flex-[32%] lg:flex-[23%] max-w-[300px] shadow-md rounded-md overflow-hidden"
+              >
+                <article>
+                  {/* image container */}
+                  <header className="w-full h-[180px]">
+                    <img
+                      src={country.flags.png}
+                      alt={country.name.common}
+                      loading="lazy"
+                      className="w-full h-full object-cover object-center"
+                    />
+                  </header>
+                  <main className="bg-white text-lightText dark:bg-darkElement dark:text-darkText p-6">
+                    <h2 className="text-center font-bold text-[17px] mb-2">
+                      {country.name.common}
+                    </h2>
+                    {country?.population! && (
+                      <div className={styles.flexItems}>
+                        <p className="font-bold">Population: </p>
+                        <p>{country.population.toLocaleString() as string}</p>
+                      </div>
+                    )}
+                    {country?.region && (
+                      <div className={styles.flexItems}>
+                        <p className="font-bold">Region: </p>
+                        <p>{country.region}</p>
+                      </div>
+                    )}
+                    {country?.capital?.length > 0 && (
+                      <div className={styles.flexItems}>
+                        <p className="font-bold">Capital: </p>
+                        {country?.capital?.map(
+                          (capital: string, index: number) => {
+                            return (
+                              <p key={index}>
+                                {capital}
+                                {index === country.capital.length - 1 ? (
+                                  ""
+                                ) : (
+                                  <span className="ml-2">,</span>
+                                )}
+                              </p>
+                            );
+                          }
+                        )}
+                      </div>
+                    )}
+                  </main>
+                </article>
+              </Link>
+            );
+          })}
+        </div>
+      )}
     </main>
   );
 };
